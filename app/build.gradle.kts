1c385bf0aad7bc3158f59432c02196b8726a0be3
MahmoudKhai <mahmoud5airy2000@gmail.com>
2025-03-11 14:37:55 +0200

    added viewPager Adapter

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
     implementation(libs.androidx.viewpager2)
     implementation(libs.androidx.recyclerview)
 
+
     implementation(libs.ssp.android)
     implementation(libs.sdp.android)
 
