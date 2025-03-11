c4335ca3bf892e55e69cdff36c9b6e67f9af248b
MahmoudKhai <mahmoud5airy2000@gmail.com>
2025-03-11 22:44:23 +0200

    added some background images

--- app/src/main/java/com/mahmoud/altasherat/onBoarding/ViewPagerFragment.kt
+++ app/src/main/java/com/mahmoud/altasherat/onBoarding/ViewPagerFragment.kt
@@ -29,6 +29,11 @@ class ViewPagerFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initializeViews()
+    }
+
     private fun initializeViews() {
         tabLayout = binding.tabLayout
         setupViewPager()
