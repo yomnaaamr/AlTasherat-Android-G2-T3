254809f817e978139ed49dbeb06046eb151d2151
MahmoudKhai <mahmoud5airy2000@gmail.com>
2025-03-11 16:35:57 +0200

    added viewPager Adapter with tabLayout and custom tabs.

---
+++ app/src/main/java/com/mahmoud/altasherat/onBoarding/presentation/OnBoardingContract.kt
@@ -0,0 +1,17 @@
+package com.mahmoud.altasherat.onBoarding.presentation
+
+interface OnBoardingContract {
+    data class OnBoardingState(
+        val isLoading: Boolean,
+        val exception: Exception?,
+        val action: String?
+    ) {
+        companion object {
+            fun initialState() = OnBoardingState(
+                isLoading = false,
+                exception = null,
+                action = null
+            )
+        }
+    }
+}
