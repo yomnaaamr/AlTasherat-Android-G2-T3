ada3233410895efa1773816159b119206214641e
Mahmoud Mostafa <mahmoud.mostafa20112011@gmail.com>
2025-03-17 22:53:06 +0200

    Setup tablayout inside AuthFragment

--- app/src/main/java/com/mahmoud/altasherat/features/authentecation/presentation/AuthFragment.kt
+++ app/src/main/java/com/mahmoud/altasherat/features/authentecation/presentation/AuthFragment.kt
@@ -1,13 +1,21 @@
 package com.mahmoud.altasherat.features.authentecation.presentation
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayout.OnTabSelectedListener
 import com.mahmoud.altasherat.R
+import com.mahmoud.altasherat.databinding.FragmentAuthBinding
 
 class AuthFragment : Fragment() {
+
+    private lateinit var binding: FragmentAuthBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,9 +25,85 @@ class AuthFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_auth, container, false)
+        binding = FragmentAuthBinding.inflate(inflater, container, false)
+        setupTab(binding.authTabLayout)
+
+        parentFragmentManager.beginTransaction()
+            .replace(R.id.auth_fragment_container, LoginFragment())
+            .commit()
+
+        binding.authTabLayout.addOnTabSelectedListener(object : OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+
+                val fragment = when (tab?.position) {
+                    0 -> LoginFragment()
+                    1 -> SignupFragment()
+                    else -> LoginFragment()
+                }
+                parentFragmentManager.beginTransaction()
+                    .replace(R.id.auth_fragment_container, fragment)
+                    .commit()
+                updateTabSelection(binding.authTabLayout, tab?.position ?: 0)
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {}
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {}
+
+        })
+
+        return binding.root
+    }
+
+    private fun setupTab(authTabLayout: TabLayout) {
+        val tabTitles = listOf("تسجيل الدخول", "حساب جديد")
+
+        for (i in tabTitles.indices) {
+            val tab = authTabLayout.newTab()
+            tab.customView = createCustomTabView(tabTitles[i])
+            authTabLayout.addTab(tab)
+        }
+
+    }
+
+    private fun createCustomTabView(title: String): View {
+        val view =
+            LayoutInflater.from(this.requireContext()).inflate(R.layout.custom_tab_item, null)
+        val text = view.findViewById<TextView>(R.id.tab_text)
+        text.text = title
+        return view
+    }
+
+    private fun updateTabSelection(tabLayout: TabLayout, selectedIndex: Int) {
+        for (i in 0 until tabLayout.tabCount) {
+            val tab = tabLayout.getTabAt(i)
+            val view = tab?.customView
+            val planeIcon = view?.findViewById<ImageView>(R.id.tab_image)
+            val indicator = view?.findViewById<View>(R.id.tab_indicator)
+            val text = view?.findViewById<TextView>(R.id.tab_text)
+
+            if (i == selectedIndex) {
+                planeIcon?.visibility = View.VISIBLE
+                indicator?.visibility = View.VISIBLE
+                text?.setTextColor(
+                    ContextCompat.getColor(
+                        this.requireContext(),
+                        R.color.md_theme_onSurface
+                    )
+                )
+            } else {
+                planeIcon?.visibility = View.GONE
+                indicator?.visibility = View.GONE
+                text?.setTextColor(
+                    ContextCompat.getColor(
+                        this.requireContext(),
+                        R.color.md_theme_onSurfaceVariant
+                    )
+                )
+            }
+        }
     }
 
 }
