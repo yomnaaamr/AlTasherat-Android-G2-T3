f75d4d4747eb6122e52e7a7242d529fb65cf3b0a
Yomna Amr <yomnaaamr259@gmail.com>
2025-03-21 01:38:27 +0200

    Add base fragment for common fragment functionality and message delegator for consistent error/message handling

--- app/src/main/java/com/mahmoud/altasherat/features/authentication/login/data/repository/local/LoginLocalDS.kt
+++ app/src/main/java/com/mahmoud/altasherat/features/authentication/login/data/repository/local/LoginLocalDS.kt
@@ -1,16 +1,17 @@
 package com.mahmoud.altasherat.features.authentication.login.data.repository.local
 
+import com.google.gson.Gson
 import com.mahmoud.altasherat.common.data.repository.local.StorageKeyEnum
-import com.mahmoud.altasherat.common.data.util.toJson
 import com.mahmoud.altasherat.common.domain.repository.local.ILocalStorageProvider
 import com.mahmoud.altasherat.features.authentication.login.data.models.entity.LoginEntity
 import com.mahmoud.altasherat.features.authentication.login.domain.repository.local.ILoginLocalDS
 
 class LoginLocalDS(
-    private val localStorageProvider: ILocalStorageProvider
+    private val localStorageProvider: ILocalStorageProvider,
+    private val gson: Gson
 ) : ILoginLocalDS {
     override suspend fun saveLogin(loginEntity: LoginEntity) {
-        val loginEntityAsGson = loginEntity.toJson()
+        val loginEntityAsGson = gson.toJson(loginEntity)
         localStorageProvider.save(StorageKeyEnum.LOGIN, loginEntityAsGson, String::class)
     }
 }
