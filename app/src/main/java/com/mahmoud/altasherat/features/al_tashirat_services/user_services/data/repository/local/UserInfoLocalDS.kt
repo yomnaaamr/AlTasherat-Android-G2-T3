83eef66c40a1db963d062f3d2eecae8a1904bc00
Mahmoud Mostafa <mahmoud.mostafa20112011@gmail.com>
2025-03-25 20:26:17 +0200

    Update UserLocalDS

--- app/src/main/java/com/mahmoud/altasherat/features/al_tashirat_services/user_services/data/repository/local/UserInfoLocalDS.kt
+++ app/src/main/java/com/mahmoud/altasherat/features/al_tashirat_services/user_services/data/repository/local/UserInfoLocalDS.kt
@@ -5,6 +5,7 @@ import com.mahmoud.altasherat.common.data.repository.local.StorageKeyEnum
 import com.mahmoud.altasherat.common.domain.repository.local.ILocalStorageProvider
 import com.mahmoud.altasherat.features.al_tashirat_services.user_services.data.models.entity.UserEntity
 import com.mahmoud.altasherat.features.al_tashirat_services.user_services.domain.repository.local.IUserInfoLocalDS
+import com.mahmoud.altasherat.features.profile_info.data.models.entity.UpdateAccEntity
 
 class UserInfoLocalDS(
     private val localStorageProvider: ILocalStorageProvider,
@@ -13,5 +14,10 @@ class UserInfoLocalDS(
     override suspend fun getUserInfo(): UserEntity {
         val userEntity = localStorageProvider.get(StorageKeyEnum.USER, "", String::class)
         return gson.fromJson(userEntity, UserEntity::class.java)
+    }
+
+    override suspend fun updateLocalUserInfo(updateAccEntity: UpdateAccEntity) {
+        val userJson = gson.toJson(updateAccEntity.user)
+        localStorageProvider.update(StorageKeyEnum.USER, userJson, String::class)
     }
 }
