870117d8e209b3f56b1492ec9ccfb17aa3b03b5d
Mahmoud Mostafa <mahmoud.mostafa20112011@gmail.com>
2025-04-06 15:13:29 +0200

    Create Delete account bottomSheet class

--- app/src/main/java/com/mahmoud/altasherat/features/delete_account/presentation/DeleteAccountFragment.kt
+++ app/src/main/java/com/mahmoud/altasherat/features/delete_account/presentation/DeleteAccountFragment.kt
@@ -1,14 +1,14 @@
-package com.mahmoud.altasherat.features.delete_account
+package com.mahmoud.altasherat.features.delete_account.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.findNavController
 import com.mahmoud.altasherat.common.presentation.base.BaseFragment
 import com.mahmoud.altasherat.databinding.FragmentDeleteAccountBinding
 
-
 class DeleteAccountFragment : BaseFragment<FragmentDeleteAccountBinding>(
     FragmentDeleteAccountBinding::inflate
 ) {
+    private lateinit var confirmBottomSheet: ConfirmDeleteBottomSheet
 
     override fun FragmentDeleteAccountBinding.initialize() {
         (requireActivity() as AppCompatActivity).setSupportActionBar(deleteAccToolbar)
@@ -22,7 +22,8 @@ class DeleteAccountFragment : BaseFragment<FragmentDeleteAccountBinding>(
         }
 
         confirmDeleteAccBtn.setOnClickListener {
-
+            confirmBottomSheet = ConfirmDeleteBottomSheet()
+            confirmBottomSheet.show(childFragmentManager, "Confirm delete account bottom sheet")
         }
     }
 }
