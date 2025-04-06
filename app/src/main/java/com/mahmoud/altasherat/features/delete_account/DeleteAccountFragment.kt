e7e17c079e244b44c31731d94034f4cf7c9ca671
Mahmoud Mostafa <mahmoud.mostafa20112011@gmail.com>
2025-04-06 14:54:29 +0200

    Create Delete acc bottom sheet layout

--- app/src/main/java/com/mahmoud/altasherat/features/delete_account/DeleteAccountFragment.kt
+++ app/src/main/java/com/mahmoud/altasherat/features/delete_account/DeleteAccountFragment.kt
@@ -20,5 +20,9 @@ class DeleteAccountFragment : BaseFragment<FragmentDeleteAccountBinding>(
         deleteAccToolbar.setNavigationOnClickListener {
             findNavController().navigateUp()
         }
+
+        confirmDeleteAccBtn.setOnClickListener {
+
+        }
     }
 }
