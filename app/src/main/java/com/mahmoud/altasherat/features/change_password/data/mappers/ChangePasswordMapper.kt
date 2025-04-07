a4bb21d3669bdcbf3331fc3b682870ac4974ae18
Mahmoud Mostafa <mahmoud.mostafa20112011@gmail.com>
2025-04-07 17:22:03 +0200

    Create ] Dto, Domain models and mappers

---
+++ app/src/main/java/com/mahmoud/altasherat/features/change_password/data/mappers/ChangePasswordMapper.kt
@@ -0,0 +1,13 @@
+package com.mahmoud.altasherat.features.change_password.data.mappers
+
+import com.mahmoud.altasherat.features.change_password.data.models.dto.ChangePasswordDto
+import com.mahmoud.altasherat.features.change_password.domain.models.ChangePassword
+
+internal object ChangePasswordMapper {
+    fun dtoToDomain(model: ChangePasswordDto): ChangePassword {
+        return ChangePassword(
+            message = model.message,
+        )
+    }
+
+}
---
+++ app/src/main/java/com/mahmoud/altasherat/features/change_password/domain/models/ChangePassword.kt
@@ -0,0 +1,5 @@
+package com.mahmoud.altasherat.features.change_password.domain.models
+
+data class ChangePassword(
+    private val message: String
+)
