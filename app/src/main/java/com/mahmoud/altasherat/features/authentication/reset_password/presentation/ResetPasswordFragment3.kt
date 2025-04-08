511df6cd485fad3538431b94f9f8b3cd44690e03
Yomna Amr <yomnaaamr259@gmail.com>
2025-04-08 12:43:03 +0200

    added ui layout for the last screen of reset password

---
+++ app/src/main/java/com/mahmoud/altasherat/features/authentication/reset_password/presentation/ResetPasswordFragment3.kt
@@ -0,0 +1,13 @@
+package com.mahmoud.altasherat.features.authentication.reset_password.presentation
+
+import com.mahmoud.altasherat.common.presentation.base.BaseFragment
+import com.mahmoud.altasherat.databinding.FragmentResetPassword3Binding
+
+class ResetPasswordFragment3 : BaseFragment<FragmentResetPassword3Binding>(FragmentResetPassword3Binding::inflate)  {
+
+    override fun FragmentResetPassword3Binding.initialize() {
+
+    }
+
+
+}
