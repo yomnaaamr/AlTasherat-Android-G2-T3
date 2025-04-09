77a794e40c5ededb66705d6311796b2ca4e8fb63
Mahmoud Mostafa <mahmoud.mostafa20112011@gmail.com>
2025-04-09 12:39:28 +0200

    Create DeleteAccUCTest class

---
+++ app/src/test/java/com/mahmoud/altasherat/features/delete_account/delete_acc/domain/usecase/DeleteAccountUCTest.kt
@@ -0,0 +1,24 @@
+package com.mahmoud.altasherat.features.delete_account.delete_acc.domain.usecase
+
+import com.mahmoud.altasherat.features.delete_account.delete_acc.data.repository.DeleteAccountRepository
+import com.mahmoud.altasherat.features.delete_account.delete_acc.domain.repository.remote.IDeleteAccountRemoteDS
+import org.junit.Before
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
+import org.mockito.kotlin.mock
+
+@RunWith(MockitoJUnitRunner::class)
+class DeleteAccountUCTest {
+
+    private lateinit var deleteAccountUC: DeleteAccountUC
+    private lateinit var deleteAccRepository: DeleteAccountRepository
+
+    private val deleteAccRemoteDS: IDeleteAccountRemoteDS = mock()
+
+    @Before
+    fun setUp() {
+        deleteAccRepository = DeleteAccountRepository(deleteAccRemoteDS)
+        deleteAccountUC = DeleteAccountUC(deleteAccRepository)
+    }
+
+}
