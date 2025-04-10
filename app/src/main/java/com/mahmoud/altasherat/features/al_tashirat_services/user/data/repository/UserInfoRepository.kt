5851935082810bf05819c57eec17f6179a85f192
Mahmoud Mostafa <mahmoud.mostafa20112011@gmail.com>
2025-04-10 12:41:59 +0200

    Write unit tests for GetUserTokenUC

--- app/src/main/java/com/mahmoud/altasherat/features/al_tashirat_services/user/data/repository/UserInfoRepository.kt
+++ app/src/main/java/com/mahmoud/altasherat/features/al_tashirat_services/user/data/repository/UserInfoRepository.kt
@@ -4,10 +4,8 @@ import com.mahmoud.altasherat.features.al_tashirat_services.user.data.mappers.Us
 import com.mahmoud.altasherat.features.al_tashirat_services.user.domain.models.User
 import com.mahmoud.altasherat.features.al_tashirat_services.user.domain.repository.IUserInfoRepository
 import com.mahmoud.altasherat.features.al_tashirat_services.user.domain.repository.local.IUserInfoLocalDS
-import com.mahmoud.altasherat.features.update_account.domain.repository.remote.IUpdateAccRemoteDS
 
 class UserInfoRepository(
-    private val userRemoteDS: IUpdateAccRemoteDS,
     private val userLocalDS: IUserInfoLocalDS
 ) : IUserInfoRepository {
     override suspend fun getUserInfo(): User {
