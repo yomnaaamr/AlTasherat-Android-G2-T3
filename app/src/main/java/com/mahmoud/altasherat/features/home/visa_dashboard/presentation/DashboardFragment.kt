01a10f66b468b5f240273e54f02db2c5fb677444
Yomna Amr <yomnaaamr259@gmail.com>
2025-04-15 17:11:59 +0200

    correct layout margin and added missing android entry point annotation

--- app/src/main/java/com/mahmoud/altasherat/features/home/visa_dashboard/presentation/DashboardFragment.kt
+++ app/src/main/java/com/mahmoud/altasherat/features/home/visa_dashboard/presentation/DashboardFragment.kt
@@ -4,16 +4,16 @@ import androidx.navigation.fragment.findNavController
 import com.mahmoud.altasherat.R
 import com.mahmoud.altasherat.common.presentation.base.BaseFragment
 import com.mahmoud.altasherat.databinding.FragmentDashboardBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class DashboardFragment : BaseFragment<FragmentDashboardBinding>(FragmentDashboardBinding::inflate) {
 
 
     override fun FragmentDashboardBinding.initialize() {
 
         binding.homeLayoutOne.setOnClickListener {
-//            not exist yet
-//            findNavController().navigate(R.id.action_dashboardFragment_to_formScreen)
+
         }
     }
 
